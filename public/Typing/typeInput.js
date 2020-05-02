01b3ceeda2c7f51269fb1176d263cf261e8d9f5b
Ruslan <ruslanacs93@mail.ru>
2020-05-02 17:11:27 +0300

    basic text typing

---
+++ public/Typing/typeInput.js
@@ -0,0 +1,63 @@
+class TypeInput {
+    
+    currInput = [];
+    side;
+    owner;
+
+    typeRenderer;
+    
+    textColor = { "r": 0, "g": 0, "b": 0 };
+
+    setup(inOwner, side)
+    {
+
+        this.owner = inOwner;
+        this.side = side;
+
+        if (side == 0)
+        {
+        
+            this.textColor.g = 255;
+            
+        }
+        else if (side == 1)
+        {
+        
+            this.textColor.b = 255;
+
+        }
+
+        this.typeRenderer = new TypeRenderer;
+        this.typeRenderer.setup(this.textColor);
+
+    }
+
+    updateInKey(key)
+    {
+
+        if (((key >= 'a') && (key <= 'z')) || ((key >= 'A') && (key <= 'Z')) || (key == ' '))
+        {
+            this.currInput.push(key);
+            console.log("new input");
+            console.log(this.currInput);
+
+            this.typeRenderer.setText(this.currInput);
+
+        }
+
+    }
+  
+  
+    draw()
+    {
+
+        if (this.typeRenderer != null)
+        {
+
+            this.typeRenderer.draw();
+            
+        }
+        
+    }
+  
+  }
