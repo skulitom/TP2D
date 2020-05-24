e6912faf9b86f81baa4976e8dfdf0e64f7cb47a7
skulitom <artemskulimovskiy@gmail.com>
2020-05-24 13:27:46 +0100

    typeble added

---
+++ server/Typeble.js
@@ -0,0 +1,40 @@
+class Typable {
+    constructor(id) {
+        this.id = id;
+        this.words = 'o';
+        this.typedWords = "";
+        this.textEnd = false;
+        this.fillRGB = {
+            r: 255,
+            g: 0,
+            b: 0,
+        };
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getWords() {
+        return this.words;
+    }
+
+    setFillRgb(rgb) {
+        this.fillRGB = rgb;
+    }
+
+    setTypedText(text) {
+        this.typedWords = text;
+    }
+
+    getTextEnd() {
+        return this.textEnd;
+    }
+
+    kill(killerColor) {
+        this.textEnd = true;
+        this.killerColor = killerColor;
+    }
+}
+
+module.exports = Typable;
