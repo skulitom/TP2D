53a7fd882702a20fb6adfd50f680c257cb69a1d3
skulitom <artemskulimovskiy@gmail.com>
2020-06-26 16:12:02 +0100

    user info

--- serverLobby/server.js
+++ serverLobby/server.js
@@ -7,3 +7,11 @@ let server = app.listen(PORT);
 app.get('/', (req, res) => {
     res.send('Hello Typer Space!');
 });
+
+app.get('getUserDetails/:id', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({
+        userName: 'Skulitom',
+        money: 999
+    }));
+});
