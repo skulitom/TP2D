520584d5df7ef1ef249392e3f0a8808f3905889c
skulitom <artemskulimovskiy@gmail.com>
2020-07-22 10:36:23 +0100

    setTyping clean up

--- serverGame/TypeManager.js
+++ serverGame/TypeManager.js
@@ -26,37 +26,57 @@ class TypeManager {
     };
 
     setTyping = (rgb, text, playerId) => {
-        let inText = text.join('').trim();
-        let player = this.playersList.get(playerId);
         if(this.playerTypeblesList.has(playerId)) {
-            const typeble = this.typebleList.get(this.playerTypeblesList.get(playerId));
-            if (!typeble) {
-                this.numOfTypos = 0;
-                this.playerTypeblesList.delete(playerId);
-                return consts.TM_TYPING_TYPO_NO_MATCH;
+            const noTypeble = this.checkIfPlayerHasNoTypeble(playerId);
+            if(noTypeble) {
+                return noTypeble;
             }
-            const enWords = typeble.getWords();
-            const key = typeble.getId();
-            if ((enWords === inText) && ((this.playerTypeblesList.get(playerId) === key))) {
-                return this.killTypeble(typeble, player, inText);
-            } else if (enWords.startsWith(inText) && ((this.playerTypeblesList.get(playerId) === key))) {
-                return this.partmatchTypeble(typeble, player, rgb, inText);
-            } else if (this.playerTypeblesList.get(playerId) === key) {
-                return this.typoTypeble(playerId);
-            }
-
+            return this.typebleMatchExisting(text, rgb, playerId);
         } else {
-            for (let [key, typeble] of this.typebleList.entries()) {
-                if(typeble.getWords() === inText) {
-                    return this.killTypeble(typeble, player, inText);
-                }
-                else if (typeble.getWords().startsWith(inText)) {
-                    return this.assignTypebleToPlayer(typeble, player, rgb, inText);
-                }
+            const foundTypeble = this.findNewTypeble(text, rgb, playerId);
+            if(foundTypeble || foundTypeble===0) {
+                return foundTypeble;
             }
         }
         return consts.TM_TYPING_TYPO_NO_MATCH;
 
+    };
+
+    findNewTypeble = (text, rgb, playerId) => {
+        let inText = text.join('').trim();
+        let player = this.playersList.get(playerId);
+        for (let [key, typeble] of this.typebleList.entries()) {
+            if(typeble.getWords() === inText) {
+                return this.killTypeble(typeble, player, inText);
+            }
+            else if (typeble.getWords().startsWith(inText)) {
+                return this.assignTypebleToPlayer(typeble, player, rgb, inText);
+            }
+        }
+    };
+
+    checkIfPlayerHasNoTypeble = (playerId) => {
+        const typeble = this.typebleList.get(this.playerTypeblesList.get(playerId));
+        if (!typeble) {
+            this.numOfTypos = 0;
+            this.playerTypeblesList.delete(playerId);
+            return consts.TM_TYPING_TYPO_NO_MATCH;
+        }
+    };
+
+    typebleMatchExisting = (text, rgb, playerId) => {
+        let inText = text.join('').trim();
+        let player = this.playersList.get(playerId);
+        const typeble = this.typebleList.get(this.playerTypeblesList.get(playerId));
+        const enWords = typeble.getWords();
+        const key = typeble.getId();
+        if ((enWords === inText) && ((this.playerTypeblesList.get(playerId) === key))) {
+            return this.killTypeble(typeble, player, inText);
+        } else if (enWords.startsWith(inText) && ((this.playerTypeblesList.get(playerId) === key))) {
+            return this.partmatchTypeble(typeble, player, rgb, inText);
+        } else {
+            return this.typoTypeble(playerId);
+        }
     };
 
     typoTypeble = (playerId) => {
